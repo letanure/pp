67b4c0a728ddcce7c7bc428ceeff12c82eaf4f51
Luiz Tanure <letanure@gmail.com>
2024-09-12 18:00:39 +0200

    chore: lint fix

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.ts";
+import typescriptLogo from "./typescript.svg";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const appDiv = document.querySelector<HTMLDivElement>("#app");
+if (appDiv) {
+	appDiv.innerHTML = `
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      </a>
+      <h1>Vite + TypeScript</h1>
+      <div class="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and TypeScript logos to learn more
+      </p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+  `;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const counterButton = document.querySelector<HTMLButtonElement>("#counter");
+if (counterButton) {
+	setupCounter(counterButton);
+}
